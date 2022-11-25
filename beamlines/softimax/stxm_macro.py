bcde6b7ffeb30b0a1dceded19f6672ee666e2253
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-11-25 12:37:27 +0100

    stxm macro after attempts to find the origin of extra data points in PCAP

--- beamlines/softimax/stxm_macro.py
+++ beamlines/softimax/stxm_macro.py
@@ -1,10 +1,12 @@
 from contrast.environment import env, macro, MacroSyntaxError
 from contrast.scans import SoftwareScan
 from contrast.detectors import Detector
+from contrast.motors import Motor, all_are_motors
 from contrast.recorders import active_recorders, RecorderHeader, RecorderFooter
 import numpy as np
 import time
 from collections import OrderedDict
+from tango import DeviceProxy
 
 
 @macro
@@ -12,11 +14,12 @@ class Stxm(SoftwareScan):
     """
     Sample stxm for softimax.
         
-    stxm <fast motor> <start> <stop> <intervals> <slow motor> <start> <stop> <intervals> <exp time> <latency>
+    stxm <fast motor> <start> <stop> <intervals> <slow motor> <start> <stop> <intervals> <exp time> <latency> <trigger axis X or Y>
 
     """
 
     SLEEP = .01
+    PIEZO_SYNC_DELAY = 0.5 # delay in seconds to syncronize the piezo stage with the interferometer
     FAST = 1000
     MARGIN = 1.
     PANDA = 'B318A-EA01/CTL/PandaPosTrig'
@@ -31,7 +34,7 @@ class Stxm(SoftwareScan):
 
         (self.fast_motor, self.fast_begin, self.fast_end, self.fast_ints,
          self.slow_motor, self.slow_begin, self.slow_end, self.slow_ints,
-         self.exptime, self.latency) = args
+         self.exptime, self.latency, self.trigger_axis) = args
 
         # make sure the fast motor has a velocity or proxy.velocity attribute
         ok = False
@@ -46,6 +49,23 @@ class Stxm(SoftwareScan):
             raise ValueError('Fast motor must have .velocity or .proxy.velocity attribute')
 
         super().__init__(self.exptime)
+
+                # The panda is a special device here, not just a detector.
+        if self.PANDA:
+            # real pandabox
+            self.panda = DeviceProxy(self.PANDA)
+            self.panda.DetPosCapt = False
+            self.panda.DetPosCapt = True
+            self.panda.ResetTrigCntr()
+            self.panda.ResetPointCntr()
+        else:
+            # dummy panda object
+            self.panda = DummyPanda()
+        
+        print('Trigger axis (should normally coincide with the fast scan axis) is: ', self.trigger_axis)
+
+        self._prepare_line_scan(self.fast_begin, self.fast_end, self.fast_ints+1, self.exptime, self.latency, self.trigger_axis)
+        
 
     def run(self):
         """
@@ -65,21 +85,15 @@ class Stxm(SoftwareScan):
         self._before_scan()
         self._setup()
         print('\nScan #%d starting at %s' % (self.scannr, time.asctime()))
-        
-        # The panda is a special device here, not just a detector.
-        if self.PANDA:
-            # real pandabox
-            panda = DeviceProxy(self.PANDA)
-        else:
-            # dummy panda object
-            panda = DummyPanda()
-        panda.DetPosCapt = True
 
         try:
             slow_positions = np.linspace(
-                                 self.slow_begin, self.slow_end, self.slow_ints + 1
+                                 self.slow_begin, self.slow_end, self.slow_ints+1
                              )
             for y_i, y_val in enumerate(slow_positions):
+                # enable position capturing
+                self.panda.DetPosCapt = True
+                
                 # move to the next line
                 self._before_move()
                 self.slow_motor.move(y_val)
@@ -95,28 +109,29 @@ class Stxm(SoftwareScan):
                 self.group.start(trials=10)
 
                 # run the stxm line
-                ax = 'Y' if ('y' in self.fast_motor.name) else 'X'
-                self._do_line(self.fast_motor, self.fast_begin, self.fast_end,
-                              self.fast_ints, self.exptime, self.latency, panda, ax)
+                self._do_line(self.fast_begin, self.fast_end)
 
-                while (panda.PointNOut is None
-                       or (len(panda.PointNOut) < self.fast_ints)
+                while (self.panda.PointNOut is None
+                       or (len(self.panda.PointNOut) < self.fast_ints+1)
                        or self.fast_motor.busy()
                        or self.group.busy()):
                     self._while_acquiring()
                     time.sleep(.01)
 
+                # disable position capturing to clean the data buffer
+                self.panda.DetPosCapt = False
                 # read detectors and panda
                 dt = time.time() - self.t0
                 dct = OrderedDict()
                 for d in self.group:
                     dct[d.name] = d.read()
                 dct['dt'] = dt
-                ## weird that this [:N] indexing is needed, something wrong with panda scheme?
-                dct['panda'] = {'x': panda.XPosOut[:self.fast_ints].reshape(1, -1),
-                                'y': panda.YPosOut[:self.fast_ints].reshape(1, -1),
-                                'PMT': panda.PMTOut[:self.fast_ints].reshape(1, -1),
-                                'diode': panda.PDiodeOut[:self.fast_ints].reshape(1, -1)}
+                # weird that this [:N] indexing is needed, something wrong with panda scheme?
+                dct['panda'] = {'x': self.panda.XPosOut[:self.fast_ints].reshape(1, -1),
+                                'y': self.panda.YPosOut[:self.fast_ints].reshape(1, -1),
+                                'PMT': self.panda.PMTOut[:self.fast_ints].reshape(1, -1),
+                                'diode': self.panda.PDiodeOut[:self.fast_ints].reshape(1, -1)}
+                print(f'XPosOut length is: {len(self.panda.XPosOut)}')
 
                 # pass data to recorders
                 for r in active_recorders():
@@ -159,28 +174,46 @@ class Stxm(SoftwareScan):
         else:
             self.fast_motor.velocity = vel
 
-    def _do_line(self, motor, start, end, N, exptime, latency, panda, axis):
-        panda.TrigAxis = axis # triger axis X or Y for horizontal and vertical respectively
-        panda.TrigXPos = float(start) # position in microns
-        panda.DetTimePulseStep = 1e3 * (exptime + latency)
-        panda.DetTimePulseWidth = 1e3 * exptime
-        panda.DetTimePulseN = N
-        panda.TimePulsesEnable = True
-        panda.ArmSingle()
+    def _prepare_line_scan(self, start, end, N_points, exptime, latency, trigger_axis):
+        """Prepares panda for the line acquisition"""
 
+        self.panda.TrigAxis = trigger_axis # triger axis X or Y for horizontal and vertical respectively
+        print(f'preparing line scan, moving the fast motor to {start}')
+        if trigger_axis == 'X':
+            self.fast_motor.move(start)
+            time.sleep(self.PIEZO_SYNC_DELAY)
+            self.panda.TrigXPos = self.panda.AbsX # position in microns
+        elif trigger_axis == 'Y':
+            self.fast_motor.move(start)
+            time.sleep(self.PIEZO_SYNC_DELAY)
+            self.panda.TrigYPos = self.panda.AbsY
+        else:
+            raise ValueError('The triggered axis name should be either X or Y.')
+        self.panda.DetTimePulseStep = 1e3 * (exptime + latency)
+        self.panda.DetTimePulseWidth = 1e3 * exptime
+        self.panda.DetTimePulseN = N_points
+        self.panda.TimePulsesEnable = True
+
+        # setting up trigger axis motor velocity
+        vel = (abs(start - end)) / (N_points * (exptime + latency))
+        self._set_vel(vel)
+        # self._set_vel(self.FAST)
+
+    def _do_line(self, start, end):
+        """Arms panda before each new line and moves the fast motor"""
+        
         # go to the starting position
-        self._set_vel(self.FAST)
         self.fast_motor.move(start - self.MARGIN)
+        # arm panda for a single line acquisition
+        self.panda.ArmSingle()
         while self.fast_motor.busy():
             time.sleep(self.SLEEP)
 
         # do a controlled movement
-        vel = (abs(start - end)) / (N * (exptime + latency))
-        self._set_vel(vel)
         self.fast_motor.move(end)
 
     def _while_acquiring(self):
-        print('%s: %.1f\r' % (self.fast_motor.name, self.fast_motor.user_position), end='')
+        print('%s: %.1f, point#: %i  \r' % (self.fast_motor.name, self.fast_motor.user_position, self.panda.DetPointCntr), end=' ')
 
 
 class DummyPanda(object):
@@ -195,3 +228,34 @@ class DummyPanda(object):
     def ArmSingle(self):
         pass
 
+@macro
+class InterfSync(object):
+    """
+    Syncronizes panda interferometer readings with the coarse motors positions
+
+        InterfSync [<coarse_x>] [<coarse_y>]
+    """
+    
+    PANDA = 'B318A-EA01/CTL/PandaPosTrig'
+
+    def __init__(self, coarse_x='coarse_x', coarse_y='coarse_y'):
+
+        if ('motor' in str(coarse_x.__class__) and 
+            'motor' in str(coarse_y.__class__)):
+            x_coarse_pos = coarse_x.position()
+            y_coarse_pos = coarse_y.position()
+            print(x_coarse_pos, y_coarse_pos)
+        elif coarse_x.__class__ == str and coarse_y.__class__ == str:
+            motors = {m.name: m for m in Motor.getinstances()}
+            x_coarse_pos = motors[coarse_x].position()
+            y_coarse_pos = motors[coarse_y].position()
+            print(x_coarse_pos, y_coarse_pos)
+        else:
+            raise ValueError('Valid motor objects or motor names have to be provided')
+        
+        panda = DeviceProxy(self.PANDA)
+        panda.AbsX = x_coarse_pos
+        panda.AbsY = y_coarse_pos
+
+    def run(self):
+        pass
